3708263659395b3d94489b4aedd8b59c1f4c3495
Ricky <ironest@gmail.com>
2020-09-05 17:17:39 +1000

    Refactored file/folder structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/App.css';
+import FilterCategory from './FilterCategory';
+import ListUser from './ListUser';
+
+function App() {
+
+  return (
+    <div>
+      <FilterCategory></FilterCategory>
+      <ListUser></ListUser>
+    </div>
+  );
+}
+
+export default App;
